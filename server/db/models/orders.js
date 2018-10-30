59c041f8a845eb727b81b54cf3e8aa9506c4ed2e
Jimmy Huang <jhuang5132@gmail.com>
2018-10-30 12:47:53 -0400

    added order model and removed unnecessary comments

--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -2,25 +2,11 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
+  // [{productId, price, quantity}, {productId, price, quantity}]
   items: {
     type: Sequelize.ARRAY(Sequelize.JSON),
     allowNull: false,
-    // [{productId, price, quantity}, {productId, price, quantity}] - no default value
   }
 })
 
 module.exports = Order
-
-  // price: {
-  //   type: Sequelize.DECIMAL,
-  //   allowNull: false
-  // },
-  // productId: {
-  //   type: Sequelize.INTEGER,
-  //   allowNull: false
-  // },
-  // quantity: {
-  //   type: Sequelize.STRING,
-  //   allowNull: false,
-  //   defaultValue: 0
-  // }
