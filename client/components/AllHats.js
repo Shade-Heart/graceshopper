782d4211d11b91c73815450225f788b92b0410ca
Jimmy Huang <jhuang5132@gmail.com>
2018-10-30 23:48:32 -0400

    completed search function

--- client/components/AllHats.js
+++ client/components/AllHats.js
@@ -3,17 +3,38 @@ import {Link} from 'react-router-dom'
 import AddHat from './AddHat'
 
 export default class AllHats extends React.Component {
+  state = {
+    query: ''
+  }
+
   componentDidMount() {
     this.props.loadHats()
   }
 
+  handleChange(event) {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
   render() {
     console.log(this.props)
-    const allHats = this.props.allHats
+    let allHats = this.props.allHats
+    if (allHats.length !== 0 && this.state.query !== '') {
+      allHats = allHats.filter(hat =>
+        hat.name.toLowerCase().includes(this.state.query.toLowerCase())
+      )
+    }
     return (
       <div>
         <div>
           <AddHat />
+          <input
+            type="text"
+            placeholder="Search hats"
+            onChange={this.handleChange.bind(this)}
+            value={this.state.query}
+          />
         </div>
         <ul className="list-unstyled">
           {allHats.map(hat => {
