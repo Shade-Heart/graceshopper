6c2c3a290663bec741b9a74c94afa1b1e392d20b
Paola Neira <paola1792@gmail.com>
2018-10-30 20:19:04 -0400

    with login and signup including Google authenticatation and update in welcome page

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -6,11 +6,11 @@ import {connect} from 'react-redux'
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const displayName = props.firstName !== 'NULL' ? props.firstName : props.email
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <h3>Welcome, {displayName}</h3>
     </div>
   )
 }
@@ -20,7 +20,8 @@ export const UserHome = props => {
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    email: state.user.email,
+    firstName: state.user.firstName
   }
 }
 
