2e922534c84df78f2fdcee7feb9b895d4a36d7a5
Paola Neira <paola1792@gmail.com>
2018-11-06 15:52:50 -0500

    fixed welcome page

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -20,13 +20,19 @@ export class Welcome extends React.Component {
   }
 
   render() {
+    const defaultUser = this.props.defaultUser
+
+    const isLoggedin = Object.keys(defaultUser).length !== 0
+
+    const name = isLoggedin ? this.props.defaultUser.firstName : ''
+
     return (
       <div id="welcome" className="welcome">
         <main>
           <header>
             <section className="hero">
               <div className="innerWrapper">
-                // <h2>Welcome</h2>
+                <h2>Welcome {name}</h2>
                 <h1>Welcome to the Mad Hatter</h1>
                 <h3>Find the right look for you!</h3>
               </div>
